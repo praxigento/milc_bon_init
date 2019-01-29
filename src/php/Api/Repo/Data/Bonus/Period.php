ebaa6ab910bcb783352977a781d7c0571f358a13
Alex Gusev <alex@flancer64.com>
2019-01-29 15:38:18 +0200

    MILC-28 Simple Unilevel Calc

---
+++ src/php/Api/Repo/Data/Bonus/Period.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2019
+ */
+
+namespace Praxigento\Milc\Bonus\Api\Repo\Data\Bonus;
+
+
+/**
+ * Bonus periods.
+ *
+ * @Entity
+ * @Table(name="bon_period")
+ */
+class Period
+    extends \TeqFw\Lib\Data
+{
+    const DATE_BEGIN = 'date_begin';
+    const ID = 'id';
+    const STATE = 'state';
+    const SUITE_REF = 'suite_ref';
+
+    /**
+     * @var \DateTime
+     * @Column(type="date")
+     */
+    public $date_begin;
+    /**
+     * @var int
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    public $id;
+    /**
+     * @var string
+     * @Column(type="string")
+     */
+    public $state;
+    /**
+     * @var int
+     * @Column(type="integer")
+     */
+    public $suite_ref;
+}
